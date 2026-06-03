876e784accc4002f88c4df81ef054a47f7124ff5
cheuyin <yinstanleycheung@gmail.com>
2026-06-03 14:12:02 -0700

    feat: add PATCH todo endpoint + make endpoints more robust

--- main.py
+++ main.py
@@ -7,21 +7,33 @@ app = FastAPI()
 
 
 class Todo(BaseModel):
-    id: str | None = None
+    id: str
+    title: str
+    description: str | None = None
+    is_completed: bool
+
+
+class TodoCreate(BaseModel):
+    title: str
+    description: str | None = None
+    is_completed: bool
+
+
+class TodoUpdate(BaseModel):
     title: str | None = None
     description: str | None = None
-    is_completed: bool = False
+    is_completed: bool | None = None
 
 
-todos = {}
+todos: dict[str, Todo] = {}
 
 
-@app.get("/todos")
+@app.get("/todos", response_model=list[Todo])
 def read_todos():
     return list(todos.values())
 
 
-@app.get("/todos/{todo_id}")
+@app.get("/todos/{todo_id}", response_model=Todo)
 def read_todo(todo_id: str):
     try:
         return todos[todo_id]
@@ -29,20 +41,23 @@ def read_todo(todo_id: str):
         raise HTTPException(status_code=404, detail=f"Item not found")
 
 
-@app.post("/todos")
-def create_todo(todo: Todo):
+@app.post("/todos", response_model=Todo)
+def create_todo(todo: TodoCreate):
     random_id = str(uuid.uuid4())
-    incoming_todo = jsonable_encoder(todo)
-    incoming_todo["id"] = random_id
-    todos[random_id] = incoming_todo
+    new_todo_json = todo.model_dump()
+    new_todo_json["id"] = random_id
+    new_todo = Todo(**new_todo_json)
+    todos[random_id] = new_todo
     return todos[random_id]
 
 
-# @app.patch("/todos/{todo_id}")
-# def update_todo(todo_id: str, todo: Todo):
-#     stored_todo = todos[todo_id]
-#     stored_todo_model = Todo(**stored_todo)
-#     update_data = todo.model_dump(exclude_unset=True)
-#     updated_todo = stored_todo_model.model_copy(update=update_data)
-#     todos[todo_id] = jsonable_encoder(update_todo)
-#     return updated_todo
+@app.patch("/todos/{todo_id}", response_model=Todo)
+def update_todo(todo_id: str, todo: TodoUpdate):
+    try:
+        stored_todo = todos[todo_id]
+        update_data = todo.model_dump(exclude_unset=True)
+        updated_todo = stored_todo.model_copy(update=update_data)
+        todos[todo_id] = updated_todo
+        return updated_todo
+    except KeyError:
+        raise HTTPException(status_code=404, detail="Item not found")
