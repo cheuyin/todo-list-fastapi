6d86da4ff30df1684afcf3d40954010cc44f024b
cheuyin <yinstanleycheung@gmail.com>
2026-06-07 11:07:27 -0700

    refactor: rename todoRepo to todo_repo for consistency

--- main.py
+++ main.py
@@ -7,20 +7,20 @@ from repository.todo_repository import TodoRepository
 project_root = Path(__file__).resolve().parent
 STORE_PATH = project_root / "store" / "todo.json"
 
-todoRepo = TodoRepository(STORE_PATH)
+todo_repo = TodoRepository(STORE_PATH)
 
 app = FastAPI()
 
 
 @app.get("/todos", response_model=list[Todo])
 def read_todos():
-    return todoRepo.get_all()
+    return todo_repo.get_all()
 
 
 @app.get("/todos/{todo_id}", response_model=Todo)
 def read_todo(todo_id: str):
     try:
-        return todoRepo.get(todo_id)
+        return todo_repo.get(todo_id)
     except KeyError:
         raise HTTPException(status_code=404, detail=f"Item not found")
 
@@ -31,16 +31,16 @@ def create_todo(todo: TodoCreate):
     new_todo_dict = todo.model_dump()
     new_todo_dict["id"] = new_todo_id
     new_todo = Todo(**new_todo_dict)
-    todoRepo.add(new_todo)
+    todo_repo.add(new_todo)
     return new_todo
 
 
 @app.patch("/todos/{todo_id}", response_model=Todo)
 def update_todo(todo_id: str, todo: TodoUpdate):
     try:
-        existing_todo_dict = todoRepo.get(todo_id).model_dump()
+        existing_todo_dict = todo_repo.get(todo_id).model_dump()
         existing_todo_dict.update(todo.model_dump(exclude_unset=True))
-        return todoRepo.update(Todo(**existing_todo_dict))
+        return todo_repo.update(Todo(**existing_todo_dict))
     except KeyError:
         raise HTTPException(status_code=404, detail="Item not found")
 
@@ -48,6 +48,6 @@ def update_todo(todo_id: str, todo: TodoUpdate):
 @app.delete("/todos/{todo_id}", response_model=Todo)
 def delete_todo(todo_id: str):
     try:
-        return todoRepo.delete(todo_id)
+        return todo_repo.delete(todo_id)
     except KeyError:
         raise HTTPException(status_code=404, detail="Item not found")
